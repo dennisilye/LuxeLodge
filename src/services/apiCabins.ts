f53de2f56ecb7ab1f824a2c7d9172eebf85eeebf
dennisil <dennisilye@gmail.com>
2023-09-17 20:17:38 +0300

    added react-toast

--- src/services/apiCabins.ts
+++ src/services/apiCabins.ts
@@ -1,4 +1,4 @@
-import supabase from "./supabase"
+import supabase, {supabaseUrl} from "./supabase"
 
 export const getCabins = async () => {
 
@@ -7,6 +7,65 @@ export const getCabins = async () => {
   if (error) {
     console.log(error);
     throw new Error(error.message)
+  }
+
+  return data;
+}
+
+export const deleteCabin = async (id: string) => {
+
+  const { data: cabins, error } = await supabase.from('cabins').select('id')
+
+  if (error) {
+    console.log(error);
+    throw new Error(error.message)
+  }
+
+  return cabins;
+
+}
+
+export async function createEditCabin(newCabin, id) {
+  const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
+
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replace(
+    "/",
+    ""
+  );
+  const imagePath = hasImagePath
+    ? newCabin.image
+    : `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // 1. Create/edit cabin
+  let query = supabase.from("cabins");
+
+  // A) CREATE
+  if (!id) query = query.insert([{ ...newCabin, image: imagePath }]);
+
+  // B) EDIT
+  if (id) query = query.update({ ...newCabin, image: imagePath }).eq("id", id);
+
+  const { data, error } = await query.select().single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be created");
+  }
+
+  // 2. Upload image
+  if (hasImagePath) return data;
+
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  // 3. Delete the cabin IF there was an error uplaoding image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.error(storageError);
+    throw new Error(
+      "Cabin image could not be uploaded and the cabin was not created"
+    );
   }
 
   return data;
